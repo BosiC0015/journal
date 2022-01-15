2341c5b4f5c340b666875736f6531d4790d68dc1
Bosi Chen <chenbosi.bosi@gmail.com>
2022-01-15 01:26:31 -0500

    add nav bar to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './component/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <h1>This is our Final Project</h1>
       </header>
