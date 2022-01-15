8c7335714aa4846640b62ac03b412b9dfbeace44
Bosi Chen <chenbosi.bosi@gmail.com>
2022-01-15 01:13:38 -0500

    add nav bar component

---
+++ client/src/component/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavBar.scss';
+
+export default function NavBar() {
+  return (
+    <div className='nav-bar'>
+      <div className='nav-bar__redirect'>
+        <ul className='nav-bar__redirect-link'>Today</ul>
+        <ul className='nav-bar__redirect-link'>This Week</ul>
+        <ul className='nav-bar__redirect-link'>This Month</ul>
+        <ul className='nav-bar__redirect-link'>Tracker</ul>
+      </div>
+      <div className='nav-bar__user'>
+        <ul className='nav-bar__user-action'>Log in</ul>
+        <ul className='nav-bar__user-action'>Sign up</ul>
+      </div>
+    </div>
+  );
+};
