8fc822382290e010a369a65368ff06867f1633cf
Zack3155 <zhanbin3155@gmail.com>
2022-01-17 14:54:48 -0500

    Configure Hooks and Reducers for User Authentication

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -1,40 +1,39 @@
+import axios from 'axios';
+
 import {
   useEffect,
   useReducer
 } from 'react';
 
-// Examples:
-// import dataReducer, {
-//   SET_USERS
-// } from '../reducer/data_reducer';
-// import axios from 'axios';
+import dataReducer, {
+  SET_USERS
+} from '../reducer/data_reducer';
 
-// const useApplicationData = () => {
-//   const [state, dispatch] = useReducer(dataReducer, {
-//     users: [],
-//     loading: true,
-//   });
-//   useEffect(() => {
-//     axios({
-//       method: 'GET',
-//       url: '/api/users',
-//     })
-//       .then(({
-//         data
-//       }) => {
-//         console.log(data);
-//         dispatch({
-//           type: SET_USERS,
-//           users: data
-//         });
-//       })
-//       .catch((err) => console.log(err));
-//   }, []);
+const useApplicationData = () => {
+  const [state, dispatch] = useReducer(dataReducer, {
+    users: []
+  });
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: '/api/users',
+    })
+      .then(({
+        data
+      }) => {
+        console.log(data);
+        dispatch({
+          type: SET_USERS,
+          users: data
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-//   return {
-//     state,
-//     dispatch,
-//   };
-// };
+  return {
+    state,
+    dispatch,
+  };
+};
 
 export default useApplicationData;
