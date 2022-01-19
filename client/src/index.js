ef763eb299f7a8093e3a74422e173d83a796aa66
Bosi Chen <chenbosi.bosi@gmail.com>
2022-01-19 17:31:53 -0500

    add route for /tracker page

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TrackerPage from './components/Tracker/TrackerPage';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/tracker' element={<TrackerPage />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
