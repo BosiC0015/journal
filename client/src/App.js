8ee80c91b89bbc5e83344bee813f83484c3a05a1
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2022-01-19 05:09:01 -0500

    added full calender view

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,80 @@
 
 import './App.css';
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import startOfWeek from 'date-fns/startOfWeek'
-import getDay from 'date-fns/getDay'
-import enUS from 'date-fns/locale/en-US';
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import { React, useState } from 'react';
-import NavBar from './components/NavBar/NavBar';
-
-const locales = {
-  'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
+import FullCalendar, { formatDate } from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+ import { INITIAL_EVENTS, createEventId,handleDateClick, renderEventContent, renderSidebar, handleWeekendsToggle, handleDateSelect, handleEventClick, handleEvents, renderSidebarEvent } from './event-utils'
 
 
-const events = [
-  {
-    title: "Big Meeting",
-    allDay: true,
-    start: new Date(2022, 2, 0),
-    end: new Date(2022, 2, 0)
+// import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+// import format from 'date-fns/format'
+// import parse from 'date-fns/parse'
+// import startOfWeek from 'date-fns/startOfWeek'
+// import getDay from 'date-fns/getDay'
+// import enUS from 'date-fns/locale/en-US';
+// import 'react-big-calendar/lib/css/react-big-calendar.css'
+// import DatePicker from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker.css';
+// import { React, useState } from 'react';
+ import NavBar from './components/NavBar/NavBar';
 
-  },
-  {
-    title: "Vacation",
-    start: new Date(2022, 1, 7),
-    end: new Date(2022, 1, 10)
+// const locales = {
+//   'en-US': enUS,
+// }
 
-  },
-  {
-    title: "Conference",
-    start: new Date(2022, 6, 20),
-    end: new Date(2022, 6, 23)
+// const localizer = dateFnsLocalizer({
+//   format,
+//   parse,
+//   startOfWeek,
+//   getDay,
+//   locales,
+// })
 
-  }
-]
+
+// const events = [
+//   {
+//     title: "Big Meeting",
+//     allDay: true,
+//     start: new Date(2022, 2, 0),
+//     end: new Date(2022, 2, 0)
+
+//   },
+//   {
+//     title: "Vacation",
+//     start: new Date(2022, 1, 7),
+//     end: new Date(2022, 1, 10)
+
+//   },
+//   {
+//     title: "Conference",
+//     start: new Date(2022, 6, 20),
+//     end: new Date(2022, 6, 23)
+
+//   }
+// ]
 
 function App() {
-  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
-  const [allEvents, setallEvents] = useState(events);
-
-  function handleAddEvent() {
-    setallEvents([...allEvents, newEvent])
+  
+   let state = {
+    weekendsVisible: true,
+    currentEvents: []
   }
+  
+  // const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  // const [allEvents, setallEvents] = useState(events);
+
+  // function handleAddEvent() {
+  //   setallEvents([...allEvents, newEvent])
+  // }
+
+  
   return (
     <div className="App">
       <NavBar />
-      <h1>Calendar</h1>
+      
+     
+      {/* <h1>Calendar</h1>
       <h2>Add New Event</h2>
       <div>
         <input type="text" placeholder='Add Title' style={{ width: '20%', margin: "10px" }} value={newEvent.title}
@@ -76,9 +93,36 @@ function App() {
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500, margin: "50px" }}
-      />
+      /> */}
+
+<div className='demo-app-main'>
+<FullCalendar
+  plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
+  headerToolbar={{
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+  }}
+  dateClick = {handleDateClick} // bind with an arrow function
+    
+  eventContent={renderEventContent}
+
+  //{this.handleDateClick}
+
+  initialView="dayGridMonth"
+  weekends={false}
+  events={[
+    { title: 'event 1', date: '2022-04-01' },
+    { title: 'event 2', date: '2022-04-02' }
+  ]}
+/>
+        </div>
     </div>
   );
+
 }
+
+
+
 
 export default App;
