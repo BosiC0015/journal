39c237f06bb80bec253b76f1de45aba6b33c00ac
Zack3155 <zhanbin3155@gmail.com>
2022-01-19 15:57:36 -0500

    Add Navbar to Signup Page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,12 +1,14 @@
 import useApplicationData from "../hooks/useApplicationData";
 import SignupForm from "./Forms/SignupForm";
 import "./Signup.scss"
+import NavBar from './NavBar/NavBar';
 
 export default function Signup() {
-  const { state, signup } = useApplicationData();
+  const { signup } = useApplicationData();
 
   return (
     <main>
+      <NavBar />
       <h2>Sign Up Page</h2>
       <SignupForm onSignup={signup} />
     </main>
