d8b5ade9053f2894d31b2cea316305d43350c810
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-04 22:43:05 -0500

    login form layout

--- client/src/components/Forms/LoginForm.js
+++ client/src/components/Forms/LoginForm.js
@@ -40,41 +40,45 @@ export default function LoginForm(props) {
   return (
     <section className="login">
       <h1>Login Form</h1>
-      <form
-        className="login-form"
-        autoComplete="off"
-        onKeyDown={e => e.key === 'Enter' && e.preventDefault()}
-        onSubmit={e => e.preventDefault()}>
-        <div className="email">
-          <label htmlFor="Email">Your Email </label>
-          <input
-            placeholder="Please Enter Your Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="email"
-          />
-        </div>
-        <div className="password">
-          <label htmlFor="Password">Password </label>
-          <input
-            placeholder="Please Enter Your Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-          />
-        </div>
-        <section className="login-validation">{error}</section>
-        <section className="login-button">
-          <button
-            className="login-button__confirm"
-            onClick={() => loginAccount(email, password)}
-          >Login</button>
-          <button
-            className="login-button__cancel"
-            onClick={() => cancel()}
-          >Cancel</button>
-        </section>
-      </form>
+      <div className="login-form">
+        <form
+          className="login-form__form"
+          id="login"
+          autoComplete="off"
+          onKeyDown={e => e.key === 'Enter' && e.preventDefault()}
+          onSubmit={e => e.preventDefault()}
+        >
+          <div className="email">
+            <label htmlFor="Email">Your Email </label>
+            <input
+              placeholder="Please Enter Your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+            />
+          </div>
+            <div className="password">
+            <label htmlFor="Password">Password </label>
+            <input
+              placeholder="Please Enter Your Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+            />
+          </div>
+          <div className="login-validation">{error}</div>
+          <div className="login-button">
+            <button
+              className="login-button__confirm"
+              onClick={() => loginAccount(email, password)}
+            >Login</button>
+            <button
+              className="login-button__cancel"
+              onClick={() => cancel()}
+            >Cancel</button>
+            </div>
+        </form>
+      </div>
     </section>
   );
 };
